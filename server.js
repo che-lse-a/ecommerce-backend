f2b99598e6b604fdc0fd311df99c523423cddaf7
Chelsea Nyariki <nyarikichelsea@gmail.com>
2025-11-13 23:51:16 +0300

    Added default route for home page

--- server.js
+++ server.js
@@ -17,5 +17,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Ecommerce API is running successfully 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
